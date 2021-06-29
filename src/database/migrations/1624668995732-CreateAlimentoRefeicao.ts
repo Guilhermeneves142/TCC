02c74f3f1c95bc74607973d694512215e615c7d5
Diego Guerreiro <86474656+lorddiego99@users.noreply.github.com>
2021-06-28 21:25:45 -0300

    Add files via upload

---
+++ src/database/migrations/1624668995732-CreateAlimentoRefeicao.ts
@@ -0,0 +1,27 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateAlimentoRefeicao1624668995732 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "alimento_refeicao",
+                    columns: [
+                    {
+                        name: "id_plano_alimentar_refeicao",
+                        type: "uuid"
+                    },
+                    {
+                        name: "id_alimento",
+                        type: "uuid"
+                    }
+                    ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("alimento_refeicao");
+    }
+
+}
