b308eefe7a77903002ee74360d8f3a5ada75d768
Diego Guerreiro <diegorex10@hotmail.com>
2021-07-01 21:34:18 -0300

    Update 1625182878638-FKExame.ts

--- src/database/migrations/1625182878638-FKExame.ts
+++ src/database/migrations/1625182878638-FKExame.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner, TableForeignKey} from "typeorm";
 export class FKExame1625182878638 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        /* Teste 02*/
+        /* Teste 02 03*/
         await queryRunner.createForeignKey("exame",
             new TableForeignKey({
                 name: "FKAtendimento",
