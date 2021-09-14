61ffc34577be9c6d35c5afcce615a4ea0980a5e7
Guilhermeneves142 <guilherme_n2010@hotmail.com>
2021-09-13 21:05:05 -0300

    a

---
+++ src/database/migrations/1631577885641-defaultPlanoALimentar.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class defaultPlanoALimentar1631577885641 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
